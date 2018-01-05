a8a6f0e76deb78d8bfd3e486a2e8c5b12b5b3592
SkyJinXX <Skyjin@outlook.com>
2018-01-05 13:39:43 +0800

    a

---
+++ WindowsFormsApp1/WindowsFormsApp1/A.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+    public partial class A : Form
+    {
+        public A()
+        {
+            InitializeComponent();
+        }
+
+        private void A_Load(object sender, EventArgs e)
+        {
+            string connstr = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
+            SqlConnection conn = new SqlConnection(connstr);
+            conn.Open();
+            String sql = "select * from car";
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            dataGridView1.AutoGenerateColumns = true;
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connstr = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
+            SqlConnection conn = new SqlConnection(connstr);
+            conn.Open();
+            String sql = "select * from car";
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            dataGridView1.AutoGenerateColumns = true;
+            conn.Close();
+        }
+    }
+}
